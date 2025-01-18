79eff1cb72e89c186c70c3c883f22d6400e8f8e3
muratgokmen <muratggokmen@gmail.com>
2025-01-19 00:43:54 +0300

    custom user servisten gereksiz importlar kaldırıldı

--- src/main/java/com/airline/booking/flightreservation/service/impl/CustomUserDetailService.java
+++ src/main/java/com/airline/booking/flightreservation/service/impl/CustomUserDetailService.java
@@ -1,18 +1,12 @@
 package com.airline.booking.flightreservation.service.impl;
 
-import com.airline.booking.flightreservation.dto.AuthRequest;
-import com.airline.booking.flightreservation.dto.AuthResponse;
 import com.airline.booking.flightreservation.model.User;
 import com.airline.booking.flightreservation.repository.UserRepository;
-import com.airline.booking.flightreservation.utility.JwtTokenUtil;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
+
 
 @Service
 public class CustomUserDetailService implements UserDetailsService {
